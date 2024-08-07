72af87a227badfde0f0dad6f7a3550504f537944
chenzhixin <1120716773@qq.com>
2024-08-07 09:07:45 +0800

    completable

--- src/main/java/com/nursery/coreJava/thread/future/HelloCompletableFuture.java
+++ src/main/java/com/nursery/coreJava/thread/future/HelloCompletableFuture.java
@@ -1,7 +1,6 @@
 package com.nursery.coreJava.thread.future;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * <First CompletableFuture><br>
@@ -12,7 +11,19 @@ import java.util.concurrent.Executors;
  * @since [产品/模块版本] （可选）
  */
 public class HelloCompletableFuture {
-    public static void main(String[] args) {
-        CompletableFuture CompletableFuture = new CompletableFuture();
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        System.out.println(Thread.currentThread().getName());
+        CompletableFuture completableFuture = new CompletableFuture();
+        completableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName());
+            return "hello";
+        }, Executors.newFixedThreadPool(10)).thenApplyAsync(s -> {
+            System.out.println(s);
+            System.out.println(Thread.currentThread().getName());
+            return "hello1";
+        }).whenComplete((s, throwable) -> {
+            System.out.println(s);
+            System.out.println(Thread.currentThread().getName());
+        }).get(1000, TimeUnit.MILLISECONDS);
     }
 }
